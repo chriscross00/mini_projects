253c39bc8173ff3b7a971cc7781315cb4d4ba877
Christopher Chan <christopher.chan2014@gmail.com>
2020-07-28 00:38:24 -0400

    finish article

--- oop/main.py
+++ oop/main.py
@@ -1,24 +1,16 @@
-from decorators import timer
 from fancy_decorators import Circle
 import class_decorators
-
-
-@timer
-def waste_time(num):
-    for _ in range(num):
-        sum([i**2 for i in range(10000)])
-
 
 def main():
     # c1 = Circle(5)
     # print(c1.area)
     # c1.radius = 0
     # print(c1.area)
-    cd = class_decorators.learn_class()
-    print(cd.method())
-    print(cd.class_method())
-    print(cd.static_method())
+    # cd = class_decorators.learn_class()
 
+    print(class_decorators.learn_class.class_method())
+    print(class_decorators.learn_class.static_method())
+    print(class_decorators.learn_class.method())
 
 if __name__ == '__main__':
     print("startup message")
